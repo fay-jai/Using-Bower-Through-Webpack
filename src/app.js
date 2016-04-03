10f198ab6c1a28f85c04d754fae0f3d9d64c978b
Willson Mock <willson.mock@gmail.com>
2016-04-03 16:08:38 -0700

    Add a React component to demo

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 import $ from "jquery";
+import React from "react";
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Hello {this.props.name}</p>
+            </div>
+        );
+    }
+}
 
 $("#root").text("Loading bower_components through Webpack, using ES6 syntax!");
+
+React.render(
+    <Hello name="Willson" />,
+    document.getElementById("react-app")
+);
